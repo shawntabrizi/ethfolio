c9541ebad6335f5c3825b807cf906c782e9a560e
Shawn Tabrizi <deturbanator@gmail.com>
2017-12-05 15:28:32 -0800

    Added the ability to get the saved data

--- ethfolio.js
+++ ethfolio.js
@@ -16,7 +16,7 @@ function checkAddress() {
 
 }
 
-function token(symbol, balance, price, name, percentage) {
+function tokenConstructor(symbol, balance, price, name, percentage) {
     this.symbol = symbol;
     this.balance = balance;
     this.price = price;
@@ -65,7 +65,7 @@ async function collectTokens(balances) {
                     if (symbol in tokenTracker) {
                         tokenTracker[symbol] += tok['balance'] / (Math.pow(10, tok['tokenInfo']['decimals']))
                     } else {
-                        let tokObj = new token()
+                        let tokObj = new tokenConstructor()
                         tokObj.symbol = symbol
                         tokObj.price = price['rate']
                         tokObj.name = tok['tokenInfo']['name']
@@ -78,7 +78,7 @@ async function collectTokens(balances) {
         tokenTracker['ETH'] += balances[balance]['ETH']['balance']
     }
 
-    let ethObj = new token()
+    let ethObj = new tokenConstructor()
     ethObj.balance = tokenTracker['ETH']
     ethObj.symbol = "ETH"
     ethObj.name = "Ether"
@@ -136,7 +136,7 @@ function addInput() {
     document.getElementById('inputs').appendChild(input)
 }
 
-async function displayAllBalances() {
+async function calculateAllBalances() {
     try {
 
         var inputs = document.getElementsByClassName("address");
@@ -164,35 +164,73 @@ async function displayAllBalances() {
 
         percentageOfTotal(tokens, total)
 
-        document.getElementById("output").innerHTML += "<h4>Total Price USD: $" + (total).toFixed(2) + "</h4><br>"
-
-        var other = ["Other", 0]
-
         console.log(tokens)
 
         for (symbol in tokens) {
             token = tokens[symbol]
-            document.getElementById("output").innerHTML += token.symbol + ": " + (token.percentage * 100).toFixed(2) + "% [$" + (token.price * token.balance).toFixed(2) + "]" + "<br>"
-            if (token.percentage < .005) {
-                other[1] += token.percentage
-            } else {
-                output.push([token.symbol, token.percentage])
-            }
+            output.push([token.symbol, (token.percentage).toPrecision(4)])
         }
 
-        if (other[1] > 0) {
-            output.push(other)
-        }
-        createGraph(JSON.stringify(output));
+        console.log(tokens, addresses,total,output)
 
         global.tokens = JSON.stringify(tokens)
         global.accounts = JSON.stringify(addresses)
         global.totalUsd = JSON.stringify(total)
         global.output = JSON.stringify(output)
 
+        displayAllBalances(global.output, global.totalUsd, global.accounts)
+
     } catch (error) {
         document.getElementById("output").innerHTML = error
+        console.error(error)
     }
+}
+
+function displayAllBalances(outputJSON, totalJSON, accountsJSON) {
+
+    var output = JSON.parse(outputJSON)
+    if (totalJSON) {
+        var total = JSON.parse(totalJSON)
+        total = "$" + total.toFixed(2)
+    } else {
+        var total = "HIDDEN"
+    }
+    if (accountsJSON) {
+        var accounts = JSON.parse(accountsJSON)
+    }
+    
+
+    document.getElementById("output").innerHTML += "<h3>Total Price USD: " + total + "</h3>"
+
+    document.getElementById("output").innerHTML += "<b>Accounts: </b><br>"
+    for (account in accounts) {
+        document.getElementById("output").innerHTML += accounts[account] + "<br>"
+    }
+
+    var view = []
+    var other = ["Other", 0]
+
+    for (symbol in output) {
+        token = output[symbol][0]
+        percentage = parseFloat(output[symbol][1])
+
+        document.getElementById("output").innerHTML += "<br>" + token + ": " + (percentage * 100).toPrecision(4) + "%"
+        if (percentage < .005) {
+            other[1] += percentage
+        } else {
+            view.push([token, percentage])
+        }
+    }
+
+    console.log(other[1])
+
+    if (other[1] > 0) {
+        view.push(other)
+    }
+
+
+    createGraph(view);
+
 }
 
 
@@ -201,11 +239,9 @@ function createGraph(input) {
     var datasets = []
     var labels = []
 
-    var inputJSON = JSON.parse(input)
-
-    for (tok in inputJSON) {
-        datasets.push(inputJSON[tok][1])
-        labels.push(inputJSON[tok][0])
+    for (tok in input) {
+        datasets.push(input[tok][1])
+        labels.push(input[tok][0])
     }
 
     data = {
@@ -228,7 +264,7 @@ function createGraph(input) {
 }
 
 async function submitToForm() {
-    if (global.tokens != "" && global.tokens.length < 20000) {
+    if (global.tokens != "" && global.output.length < 20000) {
         form = {
             formId: "1FAIpQLScUslukowSoxtDokclveTORdhmlEUHV1lTcaESbTCguuzwZxw",
             percentage: "500393463",
@@ -253,11 +289,90 @@ async function submitToForm() {
                     method: "post"
                 })
         } catch (error) {
-            console.log(error)
+            console.error(error)
         }
+
+        try {
+            //plz don't try to understand this, it makes no sense
+            var range = "A:A"
+            extra = "P2tleT1BSXphU3lDSlVham9"
+            extra += "XdWI0aElBdzM3dUZUaWZBMFB2emI3V2dtQ28"
+
+            var response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1EVToo4kogqGNrQ-lLgl9iXJ4759p5PCbI0HxhqCCQek/values/" + range + atob(extra + "="))
+            var sheet = await response.json()
+
+            var url = [location.protocol, '//', location.host, location.pathname].join('');
+
+            var foliourl = url + "?row=" + sheet.values.length
+
+            document.getElementById("output").innerHTML = "Your ethfolio can be found at: <a href='" + foliourl + "'>" + foliourl + "</a>"  
+
+        } catch (error) {
+            console.error(error)
+        }
+
+
 
         console.log("Done!")
     } else {
-        console.log("Length Problem?")
+        document.getElementById("output").innerHTML = "Make sure to load your address first!"
+    }
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+async function getRow(rowNumber) {
+    try {
+
+        //plz don't try to understand this, it makes no sense
+        var range = "A" + rowNumber + ":D" + rowNumber
+        extra = "P2tleT1BSXphU3lDSlVham9"
+        extra += "XdWI0aElBdzM3dUZUaWZBMFB2emI3V2dtQ28"
+
+        var response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1EVToo4kogqGNrQ-lLgl9iXJ4759p5PCbI0HxhqCCQek/values/" + range + atob(extra + "="))
+        var sheet = await response.json()
+
+        var values = sheet.values[0]
+
+        console.log(values)
+
+        if (values.length >= 2 && values.length < 5) {
+            var time = values[0]
+            var percentage = values[1]
+            if (values[2]) {
+                var totalUSD = values[2]
+            }
+            if (values[3]) {
+                var addresses = values[3]
+            }
+        } else {
+            console.log("Hmm... something wrong with google sheet response.")
+        }
+
+        displayAllBalances(percentage, totalUSD, addresses)
+
+    } catch (error) {
+        console.error(error)
+        document.getElementById("output").innerHTML = error
+    }
+}
+
+window.onload = function () {
+    var row = getParameterByName('row')
+    if (row) {
+        if (row > 1) {
+            console.log("Getting Row " + row)
+            getRow(row)
+        } else {
+            document.getElementById("output").innerHTML = "Not a valid row"
+        }
     }
 }
