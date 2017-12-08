68f6a543e4bf866c24df4400cbc46038843c59f5
Shawn Tabrizi <deturbanator@gmail.com>
2017-12-08 00:39:28 -0800

    Adding Bootstrap, styling, and logos
    Making it look good :)

--- ethfolio.js
+++ ethfolio.js
@@ -126,25 +126,26 @@ function percentageOfTotal(tokens, total) {
     }
 }
 
+function showSubmission() {
+    document.getElementById("submission").style.display = "unset"
+}
+
 function addInput() {
-    var br = document.createElement("br")
     var input = document.createElement("input")
     input.setAttribute("type", "text")
-    input.setAttribute("size", "70")
-    input.setAttribute("class", "address")
-    document.getElementById('inputs').appendChild(br);
+    input.setAttribute("class", "address form-control")
     document.getElementById('inputs').appendChild(input)
 }
 
-async function calculateAllBalances() {
+async function calculateAllBalances(addresses = [], newAddress = true) {
     try {
+        if (addresses.length == 0) {
+            var inputs = document.getElementsByClassName("address");
 
-        var inputs = document.getElementsByClassName("address");
-        var addresses = []
-
-        for (input in inputs) {
-            if (typeof inputs[input].value !== "undefined" && inputs[input].value !== "") {
-                addresses.push(inputs[input].value)
+            for (input in inputs) {
+                if (typeof inputs[input].value !== "undefined" && inputs[input].value !== "") {
+                    addresses.push(inputs[input].value)
+                }
             }
         }
 
@@ -171,14 +172,17 @@ async function calculateAllBalances() {
             output.push([token.symbol, (token.percentage).toPrecision(4)])
         }
 
-        console.log(tokens, addresses,total,output)
+        console.log(tokens, addresses, total, output)
 
         global.tokens = JSON.stringify(tokens)
         global.accounts = JSON.stringify(addresses)
         global.totalUsd = JSON.stringify(total)
         global.output = JSON.stringify(output)
 
-        displayAllBalances(global.output, global.totalUsd, global.accounts)
+        displayAllBalances(global.output, global.totalUsd, global.accounts);
+        if (newAddress) {
+            showSubmission();
+        }
 
     } catch (error) {
         document.getElementById("output").innerHTML = error
@@ -198,7 +202,7 @@ function displayAllBalances(outputJSON, totalJSON, accountsJSON) {
     if (accountsJSON) {
         var accounts = JSON.parse(accountsJSON)
     }
-    
+
 
     document.getElementById("output").innerHTML += "<h3>Total Price USD: " + total + "</h3>"
 
@@ -210,17 +214,39 @@ function displayAllBalances(outputJSON, totalJSON, accountsJSON) {
     var view = []
     var other = ["Other", 0]
 
+    var table = ""
+
+    table += `<table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Token Symbol</th>
+                            <th scope="col">Percentage</th>
+                        </tr>
+                    </thead>
+                    <tbody>`
+
     for (symbol in output) {
         token = output[symbol][0]
         percentage = parseFloat(output[symbol][1])
 
-        document.getElementById("output").innerHTML += "<br>" + token + ": " + (percentage * 100).toPrecision(4) + "%"
+        table += `<tr>
+                        <th scope="row">${parseInt(symbol) + 1}</th>
+                        <td>${token}</td>
+                        <td>${(percentage * 100).toPrecision(4) + "%"}</td>
+                    </tr>`
+
         if (percentage < .005) {
             other[1] += percentage
         } else {
             view.push([token, percentage])
         }
     }
+
+    table += `</tbody>
+                </table>`
+
+    document.getElementById("output").innerHTML = table
 
     console.log(other[1])
 
@@ -233,8 +259,31 @@ function displayAllBalances(outputJSON, totalJSON, accountsJSON) {
 
 }
 
+function createLegend (chart) {
+    var text = [];
+    text.push('<ul class="' + chart.id + '-legend">');
+
+    var data = chart.data;
+    var datasets = data.datasets;
+    var labels = data.labels;
+
+    if (datasets.length) {
+        for (var i = 0; i < datasets[0].data.length; ++i) {
+            text.push('<li><span style="background-color:' + datasets[0].backgroundColor[i] + '"></span>');
+            if (labels[i]) {
+                text.push(labels[i] + ' (' + datasets[0].data[i] + '%)');
+            }
+            text.push('</li>');
+        }
+    }
+    text.push('</ul>');
+    return text.join('');
+}
+
 
 function createGraph(input) {
+
+    document.getElementById("chartArea").style.display = "unset"
 
     var datasets = []
     var labels = []
@@ -255,67 +304,138 @@ function createGraph(input) {
         labels: labels
     };
 
+    var usdtext = '$' + parseFloat(global.totalUsd).toLocaleString(undefined, { maximumFractionDigits: 2 })
+
     var ctx = document.getElementById("chart").getContext('2d');
     var myDoughnutChart = new Chart(ctx, {
         type: 'doughnut',
-        data: data
+        data: data,
+        options: {
+            legendCallback: function (chart) {
+                return createLegend(chart);
+            },
+            legend: {
+                display: false
+            },
+            maintainAspectRatio: false,
+            responsive: true,
+            elements: {
+                center: {
+                    text: usdtext,
+                    fontStyle: 'Helvetica',
+                    sidePadding: 15
+                }
+            }
+        }
     });
+
+    //var legend = myDoughnutChart.generateLegend();
+    //document.getElementById("legend").innerHTML = legend;
 
 }
 
+Chart.pluginService.register({
+    beforeDraw: function (chart) {
+        if (chart.config.options.elements.center) {
+            //Get ctx from string
+            var ctx = chart.chart.ctx;
+
+            //Get options from the center object in options
+            var centerConfig = chart.config.options.elements.center;
+            var fontStyle = centerConfig.fontStyle || 'Arial';
+            var txt = centerConfig.text;
+            var color = centerConfig.color || '#000';
+            var sidePadding = centerConfig.sidePadding || 20;
+            var sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+            //Start with a base font of 30px
+            ctx.font = "30px " + fontStyle;
+
+            //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+            var stringWidth = ctx.measureText(txt).width;
+            var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+            // Find out how much the font can grow in width.
+            var widthRatio = elementWidth / stringWidth;
+            var newFontSize = Math.floor(30 * widthRatio);
+            var elementHeight = (chart.innerRadius * 2);
+
+            // Pick a new font size so it will not be larger than the height of label.
+            var fontSizeToUse = Math.min(newFontSize, elementHeight);
+
+            //Set font settings to draw it correctly.
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+            var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+            ctx.font = fontSizeToUse + "px " + fontStyle;
+            ctx.fillStyle = color;
+
+            //Draw text in center
+            ctx.fillText(txt, centerX, centerY);
+        }
+    }
+});
+
 async function submitToForm() {
-    if (global.tokens != "" && global.output.length < 20000) {
-        form = {
-            formId: "1FAIpQLScUslukowSoxtDokclveTORdhmlEUHV1lTcaESbTCguuzwZxw",
-            percentage: "500393463",
-            totalUsd: "1940177998",
-            accounts: "143080195"
-        }
+    var shareConfirmation
 
-        let entries = ""
-
-        entries += "&entry." + form.percentage + "=" + global.output
-        if (document.getElementById("saveTotal").checked) {
-            entries += "&entry." + form.totalUsd + "=" + global.totalUsd
-        }
-        if (document.getElementById("saveAddress").checked) {
-            entries += "&entry." + form.accounts + "=" + global.accounts
-        }
-
-        try {
-            console.log("Disregard the error related to 'Access-Control-Allow-Origin', submission should work.")
-            var response = await fetch("https://docs.google.com/forms/d/e/" + form.formId + "/formResponse?" + entries,
-                {
-                    method: "post"
-                })
-        } catch (error) {
-            console.error(error)
-        }
-
-        try {
-            //plz don't try to understand this, it makes no sense
-            var range = "A:A"
-            extra = "P2tleT1BSXphU3lDSlVham9"
-            extra += "XdWI0aElBdzM3dUZUaWZBMFB2emI3V2dtQ28"
-
-            var response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1EVToo4kogqGNrQ-lLgl9iXJ4759p5PCbI0HxhqCCQek/values/" + range + atob(extra + "="))
-            var sheet = await response.json()
-
-            var url = [location.protocol, '//', location.host, location.pathname].join('');
-
-            var foliourl = url + "?row=" + sheet.values.length
-
-            document.getElementById("output").innerHTML = "Your ethfolio can be found at: <a href='" + foliourl + "'>" + foliourl + "</a>"  
-
-        } catch (error) {
-            console.error(error)
-        }
-
-
-
-        console.log("Done!")
+    if (document.getElementById("saveAddress").checked) {
+        shareConfirmation = confirm("Are you sure you want to share your Ethereum Address?");
     } else {
-        document.getElementById("output").innerHTML = "Make sure to load your address first!"
+        shareConfirmation = true
+    }
+
+    if (shareConfirmation) {
+        if (global.tokens != "" && global.output.length < 20000) {
+            form = {
+                formId: "1FAIpQLScUslukowSoxtDokclveTORdhmlEUHV1lTcaESbTCguuzwZxw",
+                percentage: "500393463",
+                totalUsd: "1940177998",
+                accounts: "143080195"
+            }
+
+            let entries = ""
+
+            entries += "&entry." + form.percentage + "=" + global.output
+            if (document.getElementById("saveTotal").checked) {
+                entries += "&entry." + form.totalUsd + "=" + global.totalUsd
+            }
+            if (document.getElementById("saveAddress").checked) {
+                entries += "&entry." + form.accounts + "=" + global.accounts
+            }
+
+            try {
+                var range = "A:A"
+
+                var sheet = await getSheetRange(range)
+
+                var url = [location.protocol, '//', location.host, location.pathname].join('');
+
+                var foliourl = url + "?row=" + (sheet.values.length + 1)
+
+                document.getElementById("shareLink").innerHTML = `<div class="alert alert-success" role="alert">Your ethfolio can be found at: <a href="${foliourl}">${foliourl}</a></div>`
+
+            } catch (error) {
+                console.error(error)
+
+            }
+
+            try {
+                console.log("Disregard the error related to 'Access-Control-Allow-Origin', submission should work.")
+                var response = await fetch("https://docs.google.com/forms/d/e/" + form.formId + "/formResponse?" + entries,
+                    {
+                        method: "post"
+                    })
+            } catch (error) {
+                console.error(error)
+            }
+
+            console.log("Done!")
+        } else {
+            document.getElementById("output").innerHTML = "Make sure to load your address first!"
+        }
+    } else {
+        console.log("Didn't save your information.")
     }
 }
 
@@ -329,16 +449,22 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+async function getSheetRange(range)
+{
+    extra = "P2tleT1BSXphU3lDSlVham9"
+    extra += "XdWI0aElBdzM3dUZUaWZBMFB2emI3V2dtQ28"
+
+    var response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1EVToo4kogqGNrQ-lLgl9iXJ4759p5PCbI0HxhqCCQek/values/" + range + atob(extra + "="))
+    var sheet = await response.json()
+
+    return sheet
+}
+
 async function getRow(rowNumber) {
     try {
-
-        //plz don't try to understand this, it makes no sense
         var range = "A" + rowNumber + ":D" + rowNumber
-        extra = "P2tleT1BSXphU3lDSlVham9"
-        extra += "XdWI0aElBdzM3dUZUaWZBMFB2emI3V2dtQ28"
 
-        var response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1EVToo4kogqGNrQ-lLgl9iXJ4759p5PCbI0HxhqCCQek/values/" + range + atob(extra + "="))
-        var sheet = await response.json()
+        var sheet = await getSheetRange(range)
 
         var values = sheet.values[0]
 
@@ -357,7 +483,13 @@ async function getRow(rowNumber) {
             console.log("Hmm... something wrong with google sheet response.")
         }
 
-        displayAllBalances(percentage, totalUSD, addresses)
+        if (addresses) {
+            console.log('recalculating')
+            console.log(addresses)
+            calculateAllBalances(JSON.parse(addresses), false)
+        } else {
+            displayAllBalances(percentage, totalUSD, addresses)
+        }
 
     } catch (error) {
         console.error(error)
@@ -367,6 +499,7 @@ async function getRow(rowNumber) {
 
 window.onload = function () {
     var row = getParameterByName('row')
+    var a = getParameterByName('a')
     if (row) {
         if (row > 1) {
             console.log("Getting Row " + row)
@@ -374,5 +507,10 @@ window.onload = function () {
         } else {
             document.getElementById("output").innerHTML = "Not a valid row"
         }
+    }
+
+    if (a) {
+        var addresses = a.split(',')
+        calculateAllBalances(addresses)
     }
 }
